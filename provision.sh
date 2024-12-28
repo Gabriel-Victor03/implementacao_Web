13b41ef47a42fe4316a52b166e072331f9306530
Adauto Turibio <adautoturibio73@gmail.com>
2024-12-28 17:46:46 -0300

    Provisionamento

---
+++ provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Atualiza o sistema operacional
+apt-get update
+# apt-get upgrade -y
+
+# Instala pacotes necessários
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Adiciona o repositório do Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+# Atualiza novamente os pacotes para incluir o repositório do Docker
+apt-get update
+
+# Instala o Docker e o Docker Compose
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Adiciona o usuário 'vagrant' ao grupo docker (para evitar uso de 'sudo' com Docker)
+usermod -aG docker vagrant
+
+# Habilita o Docker para iniciar automaticamente ao boot
+systemctl enable docker
+
+# Confirma instalação do Docker e Docker Compose
+docker --version
+docker compose version
+
+echo "Provisionamento concluído!"
